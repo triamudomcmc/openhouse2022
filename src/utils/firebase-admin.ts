06b76fba7598e95cd0293cc06ae0cb036eb7dbb6
betich <betich365@gmail.com>
2021-12-16 18:17:57 +0700

    :sparkles: init firebase and tailwindcss

---
+++ src/utils/firebase-admin.ts
@@ -0,0 +1,18 @@
+import admin from "firebase-admin"
+
+export function getDb() {
+  try {
+    return admin.firestore()
+  } catch {
+    admin.initializeApp({
+      credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/gm, "\n"),
+      }),
+      //   databaseURL: "https://tocpc-prod-2021.firebaseio.com",
+      //   storageBucket: "tocpc-prod-2021.appspot.com",
+    })
+    return admin.firestore()
+  }
+}
---
+++ src/utils/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCPxDbeMohQVVhjii__DAhYdCd1-Wjmmdk",
+  authDomain: "triamoph2022-dev.firebaseapp.com",
+  projectId: "triamoph2022-dev",
+  storageBucket: "triamoph2022-dev.appspot.com",
+  messagingSenderId: "39894632284",
+  appId: "1:39894632284:web:54de75b4745f9147b26399",
+  measurementId: "G-MWRVZ45DHD",
+}
+
+const firebaseApp = initializeApp(firebaseConfig)
+
+export default firebaseApp
