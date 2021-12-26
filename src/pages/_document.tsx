7acd6a7e61cfd73e7764666d573170681880fdb9
betich <betich365@gmail.com>
2021-12-26 23:14:30 +0700

    add favicons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -19,6 +19,13 @@ export default class Document extends NextDocument {
             href="https://fonts.googleapis.com/css2?family=Noto+Sans+Thai:wght@300;400;500;600;700&family=Prompt:wght@500&display=swap?family=Roboto+Mono&display=swap"
             rel="stylesheet"
           />
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
           <Main />
           <NextScript />
         </body>
