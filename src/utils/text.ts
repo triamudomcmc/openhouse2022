17840891f0ae3d02277bd2ee575a7699e658c187
ImSoZRious <30285202+ImSoZRious@users.noreply.github.com>
2022-01-04 04:09:38 +0700

    Fixed topPrimary always empty

--- src/utils/text.ts
+++ src/utils/text.ts
@@ -5,7 +5,7 @@ const correction = {
   "์ี": "ี์", "่ี": "ี่", "้ี": "ี้", "๊ี": "ี๊", "๋ี": "ี๋", "็ี": "ี็",
   "์ึ": "ึ์", "่ึ": "ึ่", "้ึ": "ึ้", "๊ึ": "ึ๊", "๋ึ": "ึ๋", "็ึ": "ึ็",
   "ิุ์": "ุิ์", "์ุิ": "ุิ์", "์ิุ": "ุิ์", "ิ์ุ": "ุิ์", "ุ์ิ": "ุิ์"
-  , "เเ": "แ","ํา": "ำ"
+  , "เเ": "แ", "ํา": "ำ"
 }
 
 export const fixGrammar = (text: string) => {
@@ -15,23 +15,25 @@ export const fixGrammar = (text: string) => {
     fixed = fixed.replace(new RegExp(item, "g"), correction[item])
   })
 
-  return fixed.replace(/\u200B/g,'').trim()
+  return fixed.replace(/\u200B/g, '').trim()
 }
 
 
 export const searchKeyword = (arr: any, keyword: string, keySelector: (obj: any) => string) => {
-  let topPrimary: any = [],topSecondary: any = [], bottom: any = []
+  let topPrimary: any = [], topSecondary: any = [], bottom: any = []
   const keyLength = keyword.length
+
   arr.forEach((val: any) => {
-    if (fixGrammar(keyword.toLowerCase()) === fixGrammar(keySelector(val).slice(0, keyLength).toLowerCase())) return topPrimary.push(val)
-    if (fixGrammar(keySelector(val).toLowerCase()).includes(fixGrammar(keyword.toLowerCase()))) return topSecondary.push(val)
+    if (fixGrammar(keyword.toLowerCase()) === fixGrammar(keySelector(val).substring(4).slice(0, keyLength).toLowerCase())) return topPrimary.push(val)
+    if (fixGrammar(keySelector(val).substring(4).toLowerCase()).includes(fixGrammar(keyword.toLowerCase()))) return topSecondary.push(val)
     return bottom.push(val)
   })
+
   return [...topPrimary, ...topSecondary, ...bottom]
 }
 
 export const objToArr = (obj: any, filter = (val: any) => val) => {
   return Object.keys(obj).map(key => {
-    return filter({clubID: key, ...obj[key]})
+    return filter({ clubID: key, ...obj[key] })
   }).filter(i => (i !== null))
 }
