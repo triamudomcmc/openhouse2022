29ddaad4329aaca0e91cef41e0f332232708abc6
betich <betich365@gmail.com>
2022-03-06 18:01:26 +0700

    null catching

--- src/components/common/Countdown.tsx
+++ src/components/common/Countdown.tsx
@@ -44,25 +44,25 @@ export const CountDown: FC<{ until?: number }> = ({ until }) => {
       <div className="flex text-center space-x-2 sm:space-x-6 text-2xl sm:text-4xl">
         <div className="flex-col space-y-2">
           <p className="font-mono px-6 py-6 bg-gray-500 bg-opacity-25 backdrop-blur-sm rounded-tl-xl rounded-bl-xl">
-            {timeLeft?.days}
+            {timeLeft?.days ?? 0}
           </p>
           <p className="text-sm sm:text-base font-light">{timeLeft?.days && timeLeft?.days > 1 ? "days" : "day"}</p>
         </div>
         <div className="flex-col space-y-2">
           <p className="font-mono px-4 py-6 bg-gray-500 bg-opacity-25 backdrop-blur-sm">
-            {String(timeLeft?.hours).padStart(2, "0)")}
+            {String(timeLeft?.hours ?? 0).padStart(2, "0)")}
           </p>
           <p className="text-sm sm:text-base font-light">hours</p>
         </div>
         <div className="flex-col space-y-2">
           <p className="font-mono px-4 py-6 bg-gray-500 bg-opacity-25 backdrop-blur-sm">
-            {String(timeLeft?.minutes).padStart(2, "0")}
+            {String(timeLeft?.minutes ?? 0).padStart(2, "0")}
           </p>
           <p className="text-sm sm:text-base font-light">minutes</p>
         </div>
         <div className="flex-col space-y-2">
           <p className="font-mono px-4 py-6 bg-gray-500 bg-opacity-25 backdrop-blur-sm rounded-tr-xl rounded-br-xl">
-            {String(timeLeft?.seconds).padStart(2, "0")}
+            {String(timeLeft?.seconds ?? 0).padStart(2, "0")}
           </p>
           <p className="text-sm sm:text-base font-light">seconds</p>
         </div>
