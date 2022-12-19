d18dd1f7326a0d5b10dd919bdcb4a3802b1701cb
P-CAST <paral.siritrakul.p@gmail.com>
2022-12-19 14:02:36 +0700

    hothothot

--- src/config/gcpConfig.ts
+++ src/config/gcpConfig.ts
@@ -2,6 +2,6 @@ export const gcpCert = {
     projectId: process.env.GCP_PROJECT_ID,
     credentials: {
         client_email: process.env.GCP_CLIENT_EMAIL,
-        private_key: process.env.GCP_PRIVATE_KEY.replace(/\\n/g, "\n"),
+        private_key: process.env.GCP_PRIVATE_KEY?.replace(/\\n/g, "\n"),
     },
 }
