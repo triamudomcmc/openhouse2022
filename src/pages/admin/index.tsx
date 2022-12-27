5666aada37d781389bf552ab1e8a6b286629cc2c
pattheera <pattheera.prair@gmail.com>
2022-12-27 16:06:21 +0700

    FINAL

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -5,6 +5,7 @@ import { useAuth } from '@lib/auth'
 import { MainRenderer } from '@components/cms/mainRender'
 import { CheckIcon, XIcon } from '@heroicons/react/outline'
 import { HamburgerButton } from '@components/common/Nav/Hamburger'
+import { Navbar } from '@components/common/Nav/Navbar'
 
 export default function AdminIndex() {
     const {user} = useAuth()
@@ -31,17 +32,9 @@ export default function AdminIndex() {
     const variants = {
         close: {
             opacity: 0,
-            // transition: {
-            //   type: "tween",
-            //   stiffness: 100,
-            // },
           },
         open: {
           opacity: 1,
-        //   transition: {
-        //     type: "tween",
-        //     stiffness: 100,
-        //   },
         },
       }
 
@@ -141,13 +134,16 @@ export default function AdminIndex() {
                             </svg>
                             <p>หน่วยงานที่มีสถานะรอการตรวจสอบ</p>
                         </div>
-                        <HamburgerButton
-                        classname='text-black'
-                        reveal={reveal}
-                        toggle={() => {
-                        setReveal(!reveal)
-                        }}
-                        />
+                        <div className='flex'>
+                            <p className='text-[26px] leading-[45px] mx-[20px]'>{focusType == 'club' ? 'ชมรม' : focusType == 'organization' ? 'องค์กรนักเรียน': focusType == 'programmes' ? 'สายการเรียน' : 'all'}</p>
+                            <HamburgerButton
+                            classname='text-black'
+                            reveal={reveal}
+                            toggle={() => {
+                            setReveal(!reveal)
+                            }}
+                            />
+                        </div>
                     </div>
                         <motion.nav
                         ref={panel}
@@ -157,13 +153,16 @@ export default function AdminIndex() {
                          >
                             <div className={`flex flex-col w-full text-black bg-white bg-opacity-90 font-display text-center`}>
                                 <div className='text-[20px] font-[500] px-[20px] py-[5px]'>
-                                    <button onClick={()=>setFocusType('club')}>ชมรม</button>
+                                    <button className='hover:underline' onClick={()=>setFocusType('')}>all</button>
                                 </div>
                                 <div className='text-[20px] font-[500] px-[20px] py-[5px]'>
-                                    <button onClick={()=>setFocusType('programme')}>สายการเรียน</button>
+                                    <button className='hover:underline' onClick={()=>setFocusType('club')}>ชมรม</button>
                                 </div>
                                 <div className='text-[20px] font-[500] px-[20px] py-[5px]'>
-                                    <button onClick={()=>setFocusType('organization')}>องค์กรนักเรียน</button>
+                                    <button className='hover:underline' onClick={()=>setFocusType('programmes')}>สายการเรียน</button>
+                                </div>
+                                <div className='text-[20px] font-[500] px-[20px] py-[5px]'>
+                                    <button className='hover:underline' onClick={()=>setFocusType('organization')}>องค์กรนักเรียน</button>
                                 </div>
                             </div>
                         </motion.nav>
@@ -173,11 +172,20 @@ export default function AdminIndex() {
                 ? <div>Loading...</div>
                 : null}
                 {pendingArticleList.map((val, key) => {
+                if(val.type == focusType || focusType == ''){
                 return (
                     <div key={val.id} className='mt-6'>
                             <div className='flex items-center lg:h-[106px] border-2 border-gray-300 rounded-[20px]'>
                                 <div className='flex justify-between w-full'>
-                                    <div className='ml-[45px]'><h5 className='lg:text-[25px] lg:leading-[50px]'>{val.nameEN}</h5></div>
+                                    <div className='ml-[45px] flex'>
+                                        {val.type == 'club' &&
+                                            <div className='flex'>
+                                                <h5 className='lg:text-[25px] lg:leading-[50px]'>{val.id}</h5>
+                                                <p className='mx-[10px] lg:text-[25px] lg:leading-[50px]'>-</p>
+                                            </div>
+                                        }
+                                        <h5 className='lg:text-[25px] lg:leading-[50px]'>{val.nameTH}</h5>
+                                    </div>
                                     <motion.div 
                                     whileHover={{ scale: 1.05 }}
                                     whileTap={{ scale: 0.95 }}
@@ -193,10 +201,16 @@ export default function AdminIndex() {
                              <div className='mt-6 border-2 border-gray-500 rounded-[22px] mx-[-50px]'>
                                 <div className='flex items-center lg:h-[106px] bg-[#3A3A3A] rounded-[20px] mb-[100px]'>
                                     <div className='flex justify-between w-full'>
-                                        <div className='ml-[45px]'><h5 className='lg:text-[25px] lg:leading-[50px] text-white'>{val.nameEN}</h5></div>
+                                        <div className='ml-[45px]'><h5 className='lg:text-[25px] lg:leading-[50px] text-white'>{val.nameTH}</h5></div>
                                         <div className='mr-[50px]'>
-                                            <button onClick={() => approve(val.id)} className='w-[52px] h-[52px] bg-[#19C57C] rounded-xl mx-3'><CheckIcon className='w-[30px] mx-auto text-white'/></button>
-                                            <button onClick={() => decline(val.id, key)} className='w-[52px] h-[52px] bg-[#E80808] rounded-xl mx-3'><XIcon className='w-[30px] mx-auto text-white'/></button>
+                                            <motion.button 
+                                            whileHover={{ scale: 1.05 }}
+                                            whileTap={{ scale: 0.95 }}
+                                            onClick={() => approve(val.id)} className='w-[52px] h-[52px] bg-[#19C57C] rounded-xl mx-3'><CheckIcon className='w-[30px] mx-auto text-white'/></motion.button>
+                                            <motion.button 
+                                            whileHover={{ scale: 1.05 }}
+                                            whileTap={{ scale: 0.95 }}
+                                            onClick={() => decline(val.id, key)} className='w-[52px] h-[52px] bg-[#E80808] rounded-xl mx-3'><XIcon className='w-[30px] mx-auto text-white'/></motion.button>
                                         </div>
                                     </div>
                                 </div>
@@ -217,7 +231,7 @@ export default function AdminIndex() {
                             </motion.div>
                             : null}
                     </div>
-                    )
+                    )}
                 })}
             </div>
         </div>
