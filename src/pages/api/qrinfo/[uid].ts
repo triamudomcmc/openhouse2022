c7ac204952e26d1ac762d841fdd6c612c589e47b
P-CAST <paral.siritrakul.p@gmail.com>
2022-12-27 18:27:38 +0700

    bring back qrinfo api

---
+++ src/pages/api/qrinfo/[uid].ts
@@ -0,0 +1,11 @@
+import { getUserData, markOnsite } from "@lib/clientDB"
+
+export default async function getInfo(req, res) {
+    const { uid } = req.query
+    if (req.headers.req_uid && req.headers.req_uid == uid) {
+        const uidData = await getUserData(uid)
+
+        if (uidData) return res.json(uidData)
+    }
+    return res.send(`Access Denied, query id: ${uid}`)
+}
---
+++ src/pages/api/qrinfo/onsite/[uid].ts
@@ -0,0 +1,16 @@
+import { getUserData, markOnsite } from '@lib/clientDB'
+import { executeOverPerm } from '@handlers/permCheck'
+
+export default async function getInfo(req, res) {
+    const { uid } = req.query
+    if (req.method == 'POST') {
+        return await executeOverPerm(req, res, ['tucmc', 'aic', 'tusc', 'clubPresident', 'clubStaff', 'teacher'], 
+            async (req, res) => {
+                const uidData = await getUserData(uid)
+                const marked = await markOnsite(uid)
+                if (uidData) return res.json(uidData,marked)
+            }
+        )
+    }
+    return res.send(304)
+}
