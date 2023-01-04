9100dca7bd4479b98ad811a51c2c43f5c76362cc
pattheera <pattheera.prair@gmail.com>
2023-01-04 16:58:33 +0700

    fix: remove article index, preview

--- src/components/cms/mainRender.tsx
+++ src/components/cms/mainRender.tsx
@@ -3,8 +3,9 @@ import { FC } from "react"
 import WinkWInk from "src/vectors/icons/winkWink"
 import ContactRenderer from "./contactRender"
 import ReviewRenderer from "./reviewsRender"
-import ImageUploader from "./imageDisplayUploader"
-import classNames from "classnames"
+// import ImageUploader from "./imageDisplayUploader"
+// import classNames from "classnames"
+import Image from "next/image"
 
 export const MainRenderer: FC<{
   // text?: string[]
@@ -46,6 +47,23 @@ export const MainRenderer: FC<{
   } else {
     text = ["ชมรมนี้ทำอะไร ?", "ประโยชน์ที่ได้รับจากการเข้าชมรม", "ผลงานของชมรม"]
   }
+
+  // const loadPicture = (order: string) => {
+  //   return info ? (
+  //     <div>
+  //       <Image
+  //         src={imagesLink ?? false ? imagesLink["first"] ?? null : null}
+  //         alt={clubArticleDes}
+  //         width={672}
+  //         height={378}
+  //         objectFit={"cover"}
+  //       />
+  //       <p className="mt-2 text-center font-texts">{clubArticleDes}</p>
+  //     </div>
+  //   ) : (
+  //     <></>
+  //   )
+  // }
 
   return (
     <div className="mb-[86px] z-10">
@@ -91,21 +109,32 @@ export const MainRenderer: FC<{
                 <WinkWInk width="70" height="70" classname="ml-[-35px] max-lg:hidden" />
               </div>
             )}
-            <h1 className="font-display font-[800] text-blue-text ml-[10px] my-auto">{text[0]}</h1>
+            <h1 className="font-display lg:text-[36px] font-[800] text-blue-text ml-[10px] my-auto">{text[0]}</h1>
             {/* <p className='text-[23px] leading-[28px] lg:text-[40px] lg:leading-[48.5px] font-[900] text-blue-text ml-[9px] lg:ml-[18px] my-auto'>?</p> */}
           </div>
           <div className="w-full mx-auto">
             <div className="lg:w-[771px] lg:h-[420px] w-[308px] h-[168px] rounded-[6px] lg:rounded-[15px] mx-auto mt-[21px] lg:mt-[36px]">
-              <ImageUploader
+              {/* <ImageUploader
                 editable={false}
                 className="rounded-[15.5px] lg:rounded-[31.2px]"
                 purpose="thumbnail"
                 link={imagesLink ?? false ? imagesLink["first"] ?? null : null}
-              />
+              /> */}
+              <div>
+                <Image
+                  src={imagesLink["first"]}
+                  alt={clubArticleDes}
+                  width={771}
+                  height={420}
+                  objectFit={"cover"}
+                  className="rounded-[15.5px] lg:rounded-[31.2px]"
+                />
+                <p className="text-center font-texts font-[300] text-xs lg:text-sm lg:leading-[20px] mt-[4px]">{clubArticleDes}</p>
+              </div> 
             </div>
-            <p className="text-center font-texts font-[300] text-xs lg:text-sm lg:leading-[20px] mt-[4px]">
+            {/* <p className="text-center font-texts font-[300] text-xs lg:text-sm lg:leading-[20px] mt-[4px]">
               {clubArticleDes}
-            </p>
+            </p> */}
           </div>
           <QuillEditor className="lg:mt-[46px] mt-[21px] lg:backdrop-blur-[4px]" value={clubArticle} readOnly={true} />
         </div>
@@ -119,7 +148,7 @@ export const MainRenderer: FC<{
                     ? "w-[194px] lg:w-[346px] shadow-[1.175px_2.35px_2.35px_rgba(0,0,0,0.25)]"
                     : type == "programmes"
                     ? "w-[279px] lg:w-[504px] shadow-[1.175px_2.35px_2.35px_rgba(0,0,0,0.25)]"
-                    : "w-[194px] lg:w-[346px] shadow-[1.175px_2.35px_2.35px_rgba(0,0,0,0.25)]"
+                    : "w-[194px] lg:w-[576px] shadow-[1.175px_2.35px_2.35px_rgba(0,0,0,0.25)]"
                 } h-[39px] l lg:h-[68px] bg-white rounded-r-[21.5px] lg:rounded-r-[36.5px] `}
           >
             {page != "admin" && (
@@ -128,20 +157,31 @@ export const MainRenderer: FC<{
                 <WinkWInk width="70" height="70" classname="ml-[-35px] max-lg:hidden" />
               </div>
             )}
-            <h1 className="font-display font-[800] text-blue-text ml-[10px] my-auto">{text[1]}</h1>
+            <h1 className="font-display lg:text-[36px] font-[800] text-blue-text ml-[10px] my-auto">{text[1]}</h1>
           </div>
           <div className="w-full mx-auto">
             <div className="lg:w-[771px] lg:h-[420px] w-[308px] h-[168px] rounded-[6px] lg:rounded-[15px] mx-auto mt-[21px] lg:mt-[36px]">
-              <ImageUploader
+              {/* <ImageUploader
                 editable={false}
                 className="rounded-[15.5px] lg:rounded-[31.2px]"
                 purpose="thumbnail"
                 link={imagesLink ?? false ? imagesLink["second"] ?? null : null}
-              />
+              /> */}
+              <div>
+                <Image
+                  src={imagesLink["second"]}
+                  alt={advantage}
+                  width={771}
+                  height={420}
+                  objectFit={"cover"}
+                  className="rounded-[15.5px] lg:rounded-[31.2px]"
+                />
+                <p className="text-center font-texts font-[300] text-xs lg:text-sm lg:leading-[20px] mt-[4px]">{advantageDes}</p>
+              </div> 
             </div>
-            <p className="text-center font-texts font-[300] text-xs lg:text-sm lg:leading-[20px] mt-[4px]">
+            {/* <p className="text-center font-texts font-[300] text-xs lg:text-sm lg:leading-[20px] mt-[4px]">
               {advantageDes}
-            </p>
+            </p> */}
           </div>
           <QuillEditor className="lg:mt-[46px] mt-[21px] lg:backdrop-blur-[4px]" value={advantage} readOnly={true} />
         </div>
@@ -164,18 +204,29 @@ export const MainRenderer: FC<{
                 <WinkWInk width="70" height="70" classname="ml-[-35px] max-lg:hidden" />
               </div>
             )}
-            <h1 className="font-display font-[800] text-blue-text ml-[10px] my-auto">{text[2]}</h1>
+            <h1 className="font-display lg:text-[36px] font-[800] text-blue-text ml-[10px] my-auto">{text[2]}</h1>
           </div>
           <div className="w-full mx-auto">
             <div className="lg:w-[771px] lg:h-[420px] w-[308px] h-[168px] rounded-[6px] lg:rounded-[15px] mx-auto mt-[21px] lg:mt-[36px]">
-              <ImageUploader
+              {/* <ImageUploader
                 editable={false}
                 className="rounded-[15.5px] lg:rounded-[31.2px]"
                 purpose="thumbnail"
                 link={imagesLink ?? false ? imagesLink["third"] ?? null : null}
-              />
+              /> */}
+              <div>
+                <Image
+                  src={imagesLink["third"]}
+                  alt={workDes}
+                  width={771}
+                  height={420}
+                  objectFit={"cover"}
+                  className="rounded-[15.5px] lg:rounded-[31.2px]"
+                />
+                <p className="text-center font-texts font-[300] text-xs lg:text-sm lg:leading-[20px] mt-[4px]">{workDes}</p>
+              </div> 
             </div>
-            <p className="text-center font-texts font-[300] text-xs lg:text-sm lg:leading-[20px] mt-[4px]">{workDes}</p>
+            {/* <p className="text-center font-texts font-[300] text-xs lg:text-sm lg:leading-[20px] mt-[4px]">{workDes}</p> */}
           </div>
           <QuillEditor className="lg:mt-[46px] mt-[21px] lg:backdrop-blur-[4px]" value={work} readOnly={true} />
         </div>
@@ -194,10 +245,84 @@ export const MainRenderer: FC<{
               <WinkWInk width="70" height="70" classname="ml-[-35px] max-lg:hidden" />
             </div>
           )}
-          <h1 className="font-display font-[800] text-blue-text ml-[10px] my-auto">รีวิวจากรุ่นพี่</h1>
+          <h1 className="font-display lg:text-[36px] font-[800] text-blue-text ml-[10px] my-auto">รีวิวจากรุ่นพี่</h1>
         </div>
         <ReviewRenderer rawData={reviews} reviewImagesLink={reviewImagesLink} editable={false} />
       </div>
     </div>
+    // <div className="max-w-6xl px-8 mx-auto">
+    //     <div className="max-w-2xl mx-auto mt-12 mb-24">
+    //       <div className="flex flex-col items-center">
+    //         <h1 className="text-5xl text-center">{info.nameTH}</h1>
+    //         <p className="text-xl">ชมรม | {info.member} คน</p>
+    //       </div>
+    //       <div className="mt-10 space-y-20">
+    //         {/* {loadPicture(0)} */}
+    //         <div>
+    //           <h1 className="text-4xl">ชมรมนี้ทำอะไร</h1>
+    //           <article
+    //             className="prose text-white font-texts mt-4 leading-[30px] text-justify article prose-inverted"
+    //           >{clubArticle}</article>
+    //         </div>
+    //         {/* {loadPicture(1)} */}
+    //         <div>
+    //           <h1 className="text-4xl">ประโยชน์ที่ได้รับจากการเข้าชมรม</h1>
+    //           <article
+    //             className="prose text-white font-texts mt-4 leading-[30px] text-justify article prose-inverted"
+    //             // dangerouslySetInnerHTML={{ __html: contents.benefit }}
+    //           >{advantage}</article>
+    //         </div>
+    //         {/* {loadPicture(2)} */}
+    //         <div>
+    //           <h1 className="text-4xl">ผลงานของชมรม</h1>
+    //           <article
+    //             className="prose text-white font-texts mt-4 leading-[30px] text-justify article prose-inverted"
+    //             // dangerouslySetInnerHTML={{ __html: contents.portfolio }}
+    //           >{work}</article>
+    //         </div>
+    //       </div>
+    //     </div>
+    //     <div className="mt-24 max-w-[50rem] mx-auto mb-24">
+    //       <h1 className="text-4xl mb-14">รีวิวจากรุ่นพี่</h1>
+    //       <div className="space-y-12">
+    //         {/* {contents.reviews.map((reviewItem: any, i: number) => (
+    //           <div key={`rev-${i}`} className="flex flex-col-reverse sm:flex-row sm:space-x-3">
+    //             <div className="flex flex-row items-start flex-shrink-0 mt-4 space-x-2 sm:flex-col sm:space-x-0 sm:mt-0">
+    //               {reviewItem.profileURL && (
+    //                 <Image
+    //                   alt={reviewItem.profileData.name}
+    //                   width={85}
+    //                   height={85}
+    //                   src={reviewItem.profileURL}
+    //                   className="rounded-2xl"
+    //                   objectFit={"cover"}
+    //                 />
+    //               )}
+    //               <div>
+    //                 <h1 className="text-2xl font-semibold text-white">{reviewItem.profileData.name}</h1>
+    //                 <p className="text-xs">เตรียมอุดม {reviewItem.profileData.year}</p>
+    //                 <p className="text-xs">
+    //                   {reviewItem.profileData.contact.startsWith("FB: ") ||
+    //                   reviewItem.profileData.contact.startsWith("Twitter: ")
+    //                     ? reviewItem.profileData.contact
+    //                     : reviewItem.profileData.contact.startsWith("IG: ")
+    //                     ? reviewItem.profileData.contact
+    //                     : `IG: ${reviewItem.profileData.contact}`}
+    //                 </p>
+    //               </div>
+    //             </div>
+    //             <div className="w-full px-6 pt-4 bg-white border border-white rounded-lg bg-opacity-20 border-opacity-60">
+    //               <span className="w-full font-light text-8xl">“</span>
+    //               <article
+    //                 className="px-8 -mt-10 prose text-justify text-white font-texts article prose-inverted"
+    //                 dangerouslySetInnerHTML={{ __html: reviewItem.content }}
+    //               ></article>
+    //               <p className="w-full -mb-10 font-light text-right text-8xl">”</p>
+    //             </div>
+    //           </div>
+    //         ))} */}
+    //       </div>
+    //     </div>
+    //   </div>
   )
 }
--- src/pages/programmes/[clubId]/index.tsx
+++ src/pages/programmes/[clubId]/index.tsx
@@ -1,8 +1,12 @@
 import { useState, useEffect } from "react"
 import { GetServerSideProps } from "next"
+import { useRouter } from "next/router"
 import Link from "next/link"
 import { useAuth } from "@lib/auth"
 import { MainRenderer } from "@components/cms/mainRender"
+import { PencilIcon } from "@heroicons/react/solid"
+import { ArrowCircleLeftIcon } from "@heroicons/react/outline"
+import { ArticleBackground } from "@vectors/background/articlebg"
 import { motion } from "framer-motion"
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
@@ -14,157 +18,79 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
 }
 
 const LandingEdit = ({ clubId }) => {
+  const router = useRouter()
   const { user } = useAuth()
+  // const [load, setLoad] = useState<boolean>(true)
   const [info, setInfo] = useState<{ [key: string]: string }>({})
-  const [status, setStatus] = useState("")
+  const [contacts, setContacts] = useState({})
+  const [clubArticle, setClubArticle] = useState("")
+  const [clubArticleDes, setClubArticleDes] = useState("")
+  const [advantage, setAdvantage] = useState("")
+  const [advantageDes, setAdvantageDes] = useState("")
+  const [work, setWork] = useState("")
+  const [workDes, setWorkDes] = useState("")
+  const [reviews, setReviews] = useState([])
+
+  const [imagesLink, setImagesLink] = useState<{ [key: string]: string }>({})
+  const [reviewImagesLink, setReviewImagesLink] = useState({})
 
   useEffect(() => {
     const fetchInitialData = async () => {
-      const permBody = JSON.stringify({ executerUid: user?.uid })
-      let dataFetch
-      if (user?.club == clubId || user?.roles?.hasOwnProperty("tucmc")) {
+
         const res = await fetch(`/api/${clubId}/handlers`, {
           method: "POST",
           body: JSON.stringify({
             executerUid: user?.uid,
-            act: "pendingcontent",
+            act: "getContent",
           }),
         })
+        const dataFetch = await res?.json()
 
-        setStatus("Pending")
-        dataFetch = await res?.json()
-      }
-
-      if (status == "") {
-        const res = await fetch(`/api/${clubId}/handlers`, {
-          method: "POST",
-          body: JSON.stringify({
-            executerUid: user?.uid,
-            act: "prodcontent",
-          }),
-        })
-        dataFetch = await res?.json()
-        setStatus("Approved")
-      }
-
-      dataFetch ? setInfo(dataFetch.Info != null ? dataFetch.Info : "") : null
+      setReviewImagesLink(dataFetch?.reviewImageUrl ?? {})
+      setImagesLink(dataFetch?.imageUrl ?? {})
+      setInfo(dataFetch.Info != null ? dataFetch.Info : "")
+      setContacts(dataFetch?.Contacts != null ? dataFetch.Contacts : {})
+      setClubArticle(dataFetch?.ClubArticle)
+      setClubArticleDes(dataFetch?.ClubArticleDes)
+      setAdvantage(dataFetch?.Advantage)
+      setAdvantageDes(dataFetch?.AdvantageDes)
+      setWork(dataFetch?.Work)
+      setWorkDes(dataFetch?.WorkDes)
+      setReviews(dataFetch?.Reviews != null ? dataFetch.Reviews : [])
     }
-    fetchInitialData()
+    if ((user?.uid && user?.club == clubId) || user?.roles?.hasOwnProperty("tucmc")) fetchInitialData()
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [user?.uid, status])
+  }, [user?.uid])
 
-  if ((user?.club == clubId || user?.roles.hasOwnProperty("tucmc")) && info)
-    return (
-      <div>
-        <div className="flex flex-col justify-center h-screen w-[310px] lg:w-[691px] mx-auto text-center">
-          <p className="text-[15.3px] leading-[18px] lg:text-[30px] lg:leading-[36px]">ข้อมูลหน่วยงาน</p>
-          <h1 className="text-[30px] leading-[36px] lg:text-[64px] lg:leading-[78px] font-[700] mt-[59.4px] lg:mt-[56px] text-blue-edit-300">
-            {info?.nameTH}
-          </h1>
-          <h1 className="text-[22.9px] leading-[28px] lg:text-[45px] lg:leading-[55px] mt-[3.23px] lg:mt-0 font-[500] opacity-60">
-            {info?.nameEN}
-          </h1>
-          <h1 className="text-[16.35px] leading-[20px] lg:text-[32px] lg:leading-[39px] mt-[4.7px] lg:mt-0 font-[500] opacity-60">
-            สายการเรียน {info?.member} คน
-          </h1>
-          <hr className="border-[0.96px] lg:border-[2px] mt-[23.6px] lg:mt-[42px]" />
-          <div className="lg:flex lg:mt-[52px] lg:h-[116px]">
-            <div className="flex flex-col justify-center text-center lg:text-left lg:w-[331px] mt-[37px] lg:mt-0">
-              <h1 className="text-[20.7px] leading-[25px] lg:text-[34px] lg:leading-[41px] font-[400]">
-                แก้ไขข้อมูลหน่วยงาน
-              </h1>
-              <p className="text-[15.2px] leading-[18.4px] lg:text-[25px] lg:leading-[30px] font-[400] lg:mt-[7px] text-[#5C5C5C] opacity-60">
-                ข้อมูลจะแสดงผลในหน้าเว็บไซต์
-              </p>
-            </div>
-            <div className="border-[2px] border-[#5C5C5C] opacity-60 max-lg:hidden" />
-            <div className="lg:w-[362px] justify-center flex flex-col">
-              <motion.div
-                className="relative flex flex-row justify-end max-lg:mt-[23.2px] max-lg:mx-auto"
-                whileHover={{ scale: 1.05 }}
-              >
-                <Link href={`/programmes/${[clubId]}/edit`}>
-                  <button className="w-[203.7px] h-[38.3px] lg:w-[335px] lg:h-[63px] bg-blue-edit-300 rounded-[13.4px] lg:rounded-[23.5px]">
-                    <p className="text-center text-[19.4px] leading-[24px] lg:text-[32px] lg:leading-[39px] font-500 text-white">
-                      แก้ไข
-                    </p>
+  if (info) return (
+      <div className='flex flex-col overflow-hidden max-lg:bg-gradient-edit lg:bg-cream'>
+          <ArticleBackground classname='max-lg:hidden' />
+          <div className='mx-auto mt-[104px] w-[311px] lg:w-[1000px] lg:mt-[305px] flex max-[1080px]:ml-[30px] z-10 '>
+              <Link href={`/clubs`}>
+                  <button className='flex'>
+                      <ArrowCircleLeftIcon className='h-[15px] w-[15px] lg:h-[30px] lg:w-[30px]' />
+                      <p className='text-xs leading-[15px] ml-[3.68px] lg:ml-[7.25px] lg:text-xl lg:leading-[29px]'>ย้อนกลับ</p>
                   </button>
-                </Link>
-              </motion.div>
-              <div className="w-[220px] lg:w-[335px] ml-[54px] lg:ml-[29px]">
-                <p className="text-left text-[16px] leading-[21px] lg:text-[24px] lg:leading-[30px] mt-[25.7px] lg:mt-[9px] text-[#5C5C5C] flex flex-row">
-                  สถานะ :
-                  <span
-                    className={`ml-[5px] ${
-                      status == "Approved"
-                        ? "text-[#19C57C]"
-                        : status == "Pending"
-                        ? "text-[#FCB52B]"
-                        : "text-[#E80808]"
-                    } flex flex-row items-center`}
-                  >
-                    <svg
-                      className="max-lg:hidden"
-                      width="15"
-                      height="15"
-                      viewBox="0 0 20 20"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <circle
-                        cx="10"
-                        cy="10"
-                        r="10"
-                        fill={`${status == "Approved" ? "#19C57C" : status == "Pending" ? "#FCB52B" : "#E80808"}`}
-                      />
-                    </svg>
-                    <svg
-                      className="lg:hidden"
-                      width="10"
-                      height="10"
-                      viewBox="0 0 20 20"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <circle
-                        cx="10"
-                        cy="10"
-                        r="10"
-                        fill={`${status == "Approved" ? "#19C57C" : status == "Pending" ? "#FCB52B" : "#E80808"}`}
-                      />
-                    </svg>
-                    <p className="ml-[3px] lg:ml-[5px]">
-                      {status == "Approved"
-                        ? "ผ่านการตรวจสอบ"
-                        : status == "Pending"
-                        ? "อยู่ระหว่างการตรวจสอบ"
-                        : "ไม่ผ่านการตรวจสอบ"}
-                    </p>
-                  </span>{" "}
-                </p>
-              </div>
-            </div>
+              </Link>
           </div>
-        </div>
+          
+          <MainRenderer
+              // editable={false}
+              type="programme"
+              info={info}
+              contacts={contacts}
+              clubArticle={clubArticle}
+              clubArticleDes={clubArticleDes}
+              advantage={advantage}
+              advantageDes={advantageDes}
+              work={work}
+              workDes={workDes}
+              reviews={reviews}
+              reviewImagesLink={reviewImagesLink}
+              imagesLink={imagesLink}
+          />
       </div>
-    )
-
-  // if (info) return (
-  //     <div className='flex flex-col max-lg:bg-gradient-edit lg:bg-cream'>
-  //         <MainRenderer
-  //             // editable={false}
-  //             info={info}
-  //             contacts={contacts}
-  //             clubArticle={clubArticle}
-  //             clubArticleDes={clubArticleDes}
-  //             advantage={advantage}
-  //             advantageDes={advantageDes}
-  //             work={work}
-  //             workDes={workDes}
-  //             reviews={reviews}
-  //         />
-  //     </div>
-  // )
+  )
 
   return (
     <>
