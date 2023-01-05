c9a2915b6199b905931e5a6257aa3bdf4fe2607d
betich <betich365@gmail.com>
2023-01-05 15:17:50 +0700

    feat: add google analytics

---
+++ src/pages/_document.tsx
@@ -0,0 +1,36 @@
+import NextDocument, { DocumentContext, Head, Html, Main, NextScript } from "next/document"
+
+export default class Document extends NextDocument {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await NextDocument.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html lang="th">
+        <Head>
+          <meta charSet="utf-8" />
+          <link rel="icon" href="/favicon.ico" />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-GX1MR96R9D"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-GX1MR96R9D', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
