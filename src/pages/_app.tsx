521089d498a593f843e34b084179a6ea4293af9b
P-CAST <paral.siritrakul.p@gmail.com>
2023-01-05 16:58:54 +0700

    add site title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,12 +6,16 @@ import { AppProps } from "next/app"
 import { Navbar } from "@components/common/Nav/Navbar"
 import { useRouter } from "next/router"
 import { Footer } from "@components/common/Footer"
+import Head from "next/head"
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter()
 
   return (
     <AuthProvider>
+      <Head>
+        <title>Triamudom Openhouse 2023</title>
+      </Head>
       <link rel="icon" href="/favicon.ico" />
       {router?.pathname !== "/ticket" && <Navbar classname=" z-[99]" />}
       <div className="min-h-screen w-full">
