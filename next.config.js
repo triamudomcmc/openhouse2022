8095e3ba8ced32960cec353db27c9c314357abbd
betich <betich365@gmail.com>
2023-01-05 09:06:30 +0700

    feat: implement vercel og rendering in place of puppetteer

--- next.config.js
+++ next.config.js
@@ -5,6 +5,17 @@ const nextConfig = {
   images: {
     domains: ["storage.googleapis.com", "lh3.googleusercontent.com"],
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.bin$/i,
+      type: "asset/resource",
+      generator: {
+        // important, otherwise it will output into a folder that is not served by next
+        filename: "static/[hash][ext][query]",
+      },
+    })
+    return config
+  },
 }
 
 module.exports = nextConfig
