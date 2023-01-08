32b2727a26aff049b793fcae1c2077d8d7ad1c8f
pattheera <pattheera.prair@gmail.com>
2023-01-08 08:30:34 +0700

    fux: spelling footer, e-stamp

--- src/pages/account/stamp/index.tsx
+++ src/pages/account/stamp/index.tsx
@@ -90,9 +90,9 @@ export default function QrGen() {
     return (
       <PageContainer>
         <div className="flex flex-col items-center text-deep-turquoise">
-          <h3 className="text-2xl font-bold mt-10">Scan here</h3>
-          <p className="font-bold -mt-2">เพื่อสะสมแสตมป์จากซุ้มต่าง ๆ</p>
-          <div className="rounded-lg my-10 w-48 h-48 bg-white">
+          <h3 className="mt-10 text-2xl font-bold">Scan here</h3>
+          <p className="-mt-2 font-bold">เพื่อสะสมแสตมป์จากซุ้มต่าง ๆ</p>
+          <div className="w-48 h-48 my-10 bg-white rounded-lg">
             <div className="mt-[6px] ml-[6px]">
               <QRCode
                 text={user?.uid}
@@ -111,7 +111,7 @@ export default function QrGen() {
             </div>
           </div>
           <div className="flex flex-col items-center bg-white rounded-3xl py-6 w-full max-w-[380px] shadow-lg">
-            <span className="font-bold text-xl mb-6 mt-4">แสตมป์ของ {user?.Info?.username}</span>
+            <span className="mt-4 mb-6 text-xl font-bold">แสตมป์ของ {user?.Info?.username}</span>
             <div className="flex flex-col space-y-3">
               {groupByN(3, stampData).map((group, index) => (
                 <div key={index} className="grid grid-cols-3 gap-2">
@@ -131,8 +131,8 @@ export default function QrGen() {
                 </div>
               ))}
             </div>
-            <p className="text-gray-500 text-center leading-5 mt-7 mb-2">
-              สะสมสแตมป์ให้ครบเพื่อแลกรับรางวัลพิเศษ <br />ณ ซุ้มคณะกรรมการกิจกรรมพัฒนาผู้เรียน (กช.)
+            <p className="mb-2 leading-5 text-center text-gray-500 mt-7">
+              สะสมแสตมป์ให้ครบเพื่อแลกรับรางวัลพิเศษ <br />ณ ซุ้มคณะกรรมการงานกิจกรรมพัฒนาผู้เรียน (กช.)
             </p>
           </div>
         </div>
