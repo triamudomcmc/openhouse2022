d19918bfbfe3b7e5ab9a33eb531aa9b545a1f2be
Peter <peterphongpak@gmail.com>
2023-01-12 19:09:13 +0700

    Update index.tsx

--- src/pages/api/ticket/index.tsx
+++ src/pages/api/ticket/index.tsx
@@ -1,7 +1,22 @@
 import screenshot from "@lib/screenshot"
 import { NextApiRequest } from "next"
 
-export default async function getTicket(req, res) {
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', false)
+  res.setHeader('Access-Control-Allow-Origin', "*");
+  res.setHeader('Access-Control-Allow-Methods', 'GET')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+async function getTicket(req, res) {
   const { uid, profileIcon, username, firstname, lastname, status, account_id } = JSON.parse(req.body)
 
   const file = await screenshot(`${getProtocol(req)}://${req.headers.host}/ticket?uid=${uid}&profileIcon=${profileIcon}&username=${username}&firstname=${firstname}&lastname=${lastname}&status=${status}&account_id=${account_id}`)
@@ -11,5 +26,8 @@ export default async function getTicket(req, res) {
   res.statusCode = 200
   res.end(file)
 }
+
+export default async allowCors(getTicket)
+
 
 const getProtocol = (req: NextApiRequest) => (req.headers.host?.includes("localhost") ? "http" : "https")
